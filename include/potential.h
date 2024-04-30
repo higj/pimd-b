8dc5ca64d6a4e0ee61feabcb03ded4c89aa22295
higj <902786+higj@users.noreply.github.com>
2024-04-30 23:36:53 +0300

    Revert back to accumulation of forces, because it happens inside the permutation loop

--- include/potential.h
+++ include/potential.h
@@ -77,13 +77,13 @@ public:
     ~DipolePotential() override = default;
 
     // Potential
-    double V(const dVec& x);
+    double V(const dVec& x) override;
 
     // Potential gradient
-    dVec gradV(const dVec& x);
+    dVec gradV(const dVec& x) override;
 
     // Potential laplacian
-    double laplacianV(const dVec& x);
+    double laplacianV(const dVec& x) override;
 
 private:
     double strength;
@@ -96,31 +96,31 @@ public:
     ~AzizPotential() override = default;
 
     // Potential
-    double V(const dVec& x);
+    double V(const dVec& x) override;
 
     // Potential gradient
-    dVec gradV(const dVec& x);
+    dVec gradV(const dVec& x) override;
 
     // Potential laplacian
-    double laplacianV(const dVec& x);
+    double laplacianV(const dVec& x) override;
 
 private:
     double rm, A, epsilon, alpha, D, C6, C8, C10;
 
     // The auxiliary F-function for the Aziz potential
-    double F(const double x) {
+    double F(const double x) const {
         return (x < D ? exp(-(D / x - 1.0) * (D / x - 1.0)) : 1.0);
     }
 
     // The derivative of the F-function
-    double dF(const double x) {
+    double dF(const double x) const {
         double ix = 1.0 / x;
         double r = 2.0 * D * ix * ix * (D * ix - 1.0) * exp(-(D * ix - 1.0) * (D * ix - 1.0));
         return (x < D ? r : 0.0);
     }
 
     // The 2nd derivative of the F-function
-    double d2F(const double x) {
+    double d2F(const double x) const {
         double ix = 1.0 / x;
         double r = 2.0 * D * ix * ix * ix * (2.0 * D * D * D * ix * ix * ix - 4.0 * D * D * ix * ix
             - D * ix + 2.0) * exp(-(D * ix - 1.0) * (D * ix - 1.0));
