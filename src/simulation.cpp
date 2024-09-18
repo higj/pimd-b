3f7c9fc61e4fe8dc799835caf258be1b638b615f
higj <902786+higj@users.noreply.github.com>
2024-09-18 21:53:15 +0300

    Comment

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -449,7 +449,8 @@ void Simulation::run() {
 }
 
 /**
- * Obtains the coordinates from the previous timeslice.
+ * Receives coordinates from the previous time-slice,
+ * and sends the current coordinates to the next time-slice.
  * 
  * @param prev Vector to store the previous coordinates.
  */
@@ -476,7 +477,8 @@ void Simulation::getPrevCoords(dVec& prev) {
 }
 
 /**
- * Obtains the coordinates from the next timeslice.
+ * Receives coordinates from the next time-slice,
+ * and sends the current coordinates to the previous time-slice.
  * 
  * @param next Vector to store the next coordinates.
  */
