6d6aa398e02ec7a25d4bc445a61d3a44ccda4149
higj <902786+higj@users.noreply.github.com>
2024-09-19 13:56:06 +0300

    Do not bother calculating empty potentials

--- src/observable.cpp
+++ src/observable.cpp
@@ -165,23 +165,26 @@ void EnergyObservable::calculateKinetic() {
  * kinetic energy of the system.
  */
 void EnergyObservable::calculatePotential() {
-    double potential = 0.0;                            // Total potential energy
-    double virial = 0.0;                               // Virial kinetic energy
-    double int_pot = 0.0;                              // Potential energy due to interactions
-    double ext_pot = sim.ext_potential->V(sim.coord);  // Potential energy due to external field
+    double potential = 0.0;  // Total potential energy
+    double virial = 0.0;     // Virial kinetic energy
+    double int_pot = 0.0;    // Potential energy due to interactions
+    double ext_pot = 0.0;    // Potential energy due to external field
 
-    potential += ext_pot;
+    if (sim.external_potential_name != "free") {
+        ext_pot = sim.ext_potential->V(sim.coord);
+        potential += ext_pot;
 
-    dVec physical_forces(sim.natoms);
-    physical_forces = (-1.0) * sim.ext_potential->gradV(sim.coord);
+        dVec physical_forces(sim.natoms);
+        physical_forces = (-1.0) * sim.ext_potential->gradV(sim.coord);
 
-    for (int ptcl_idx = 0; ptcl_idx < sim.natoms; ++ptcl_idx) {
-        for (int axis = 0; axis < NDIM; ++axis) {
-            virial -= sim.coord(ptcl_idx, axis) * physical_forces(ptcl_idx, axis);
+        for (int ptcl_idx = 0; ptcl_idx < sim.natoms; ++ptcl_idx) {
+            for (int axis = 0; axis < NDIM; ++axis) {
+                virial -= sim.coord(ptcl_idx, axis) * physical_forces(ptcl_idx, axis);
+            }
         }
     }
 
-    if (sim.int_pot_cutoff != 0.0) {
+    if (sim.interaction_potential_name != "free" && sim.int_pot_cutoff != 0.0) {
         for (int ptcl_one = 0; ptcl_one < sim.natoms; ++ptcl_one) {
             for (int ptcl_two = ptcl_one + 1; ptcl_two < sim.natoms; ++ptcl_two) {
                 dVec diff = sim.getSeparation(ptcl_one, ptcl_two, MINIM);  // Vectorial distance
@@ -201,17 +204,18 @@ void EnergyObservable::calculatePotential() {
         }
     }
 
-    potential /= sim.nbeads;
-    int_pot /= sim.nbeads;
-    ext_pot /= sim.nbeads;
-    virial *= 0.5 / sim.nbeads;
-
     if (sim.external_potential_name != "free" && sim.interaction_potential_name != "free") {
+        ext_pot /= sim.nbeads;
+        int_pot /= sim.nbeads;
+
         quantities["ext_pot"] = Units::convertToUser("energy", out_unit, ext_pot);
         quantities["int_pot"] = Units::convertToUser("energy", out_unit, int_pot);
     }
 
     if (sim.external_potential_name != "free" || sim.interaction_potential_name != "free") {
+        potential /= sim.nbeads;
+        virial *= 0.5 / sim.nbeads;
+
         quantities["potential"] = Units::convertToUser("energy", out_unit, potential);
         quantities["virial"] = Units::convertToUser("energy", out_unit, virial);
     }
