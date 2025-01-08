93d58c90ddaa629e1b01d9fd4501ab069173c1d5
Ofir Blumer <ofirblumer@compute-0-0.power>
2025-01-08 13:30:07 +0200

    print more

--- tests/main.py
+++ tests/main.py
@@ -139,13 +139,14 @@ def compare_xyz(actual_xyz_file, expected_xyz_file, natoms):
         if are_equals:
             print("scale equal!\n")
         else:
-            print(indexs, scale[indexs],expectedScale[indexs],str(actual_xyz_file).split(".")[0][-1])
+            print(indexs, format(scale[indexs], ".17e"),format(expectedScale[indexs], ".17e"),str(actual_xyz_file).split(".")[0][-1])
         scale = np.loadtxt(str(actual_xyz_file).split("output")[0] + "expfactor_" + str(actual_xyz_file).split(".")[0][-1])
         expectedScale = np.loadtxt(str(expected_xyz_file).split("pos")[0] + "expfactor_" + str(actual_xyz_file).split(".")[0][-1])
         if are_equals:
             print("scale equal!\n")
         else:
-            print(indexs, scale[indexs],expectedScale[indexs],str(actual_xyz_file).split(".")[0][-1])
+            print(indexs, format(scale[indexs], ".17e"),format(expectedScale[indexs], ".17e"),str(actual_xyz_file).split(".")[0][-1])
+
 
         #print(f"Test failed: Coordinates in {actual_xyz_file.name} do not match at step {index}, {coords[index]} {coords_test[index]}")
 #        raise AssertionError(f"Test failed: Coordinates in {actual_xyz_file.name} do not match at step {index}.")
