b3088ef1a57aeaa97389e7c627a0a854ac2043d5
higj <902786+higj@users.noreply.github.com>
2025-05-18 22:55:04 +0300

    Slightly more informative test

--- tests/main.py
+++ tests/main.py
@@ -102,7 +102,7 @@ def compare_output(actual_output, expected_output):
     for column in columns:
         are_equal, index = compare_arrays(data_actual[column], data_expected[column])
         if not are_equal:
-            raise AssertionError(f"Test failed: '{column}' does not match at step {index}.")
+            raise AssertionError(f"Test failed: '{column}' does not match at step {index}. Your value: {data_actual[column][index]}, Actual value: {data_expected[column][index]}")
         
     return True
 
