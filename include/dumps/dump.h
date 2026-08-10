b7268e03a69ccc694e758709b3a31d11d5a1d362
higj <902786+higj@users.noreply.github.com>
2026-08-10 21:08:16 +0300

    Move filesystem include from Dump source to header

--- include/dumps/dump.h
+++ include/dumps/dump.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <fstream>
+#include <filesystem>
 
 class Dump {
 public:
--- src/dumps/dump.cpp
+++ src/dumps/dump.cpp
@@ -1,7 +1,5 @@
 #include "dumps/dump.h"
 
-#include <filesystem>
-
 Dump::Dump(int this_bead, int out_freq, const std::string& out_unit) : m_this_bead(this_bead), m_out_freq(out_freq),
                                                                        m_out_unit(out_unit)
 {
